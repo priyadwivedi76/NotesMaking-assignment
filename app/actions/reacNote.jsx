8d15a7e85e091f1558d3de417ed3adb6172c0cbb
Dwivedi Priya <pd7668665@gmail.com>
2026-01-08 23:33:47 +0530

    ui-improved

--- app/actions/reacNote.jsx
+++ app/actions/reacNote.jsx
@@ -8,7 +8,21 @@ export async function getNotes() {
   return notes.map(note => ({
     ...note,
     _id: note._id.toString(),
-    createdAt: note.createdAt.toISOString(),
-    updatedAt: note.updatedAt.toISOString()
+   createdAt: new Date(note.createdAt).toLocaleString('en-IN', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true,
+    }),
+  updatedAt: new Date(note.updatedAt).toLocaleString('en-IN', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  }),
   }))
 }
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -11,17 +11,6 @@ const Navigation = () => {
           </div>
           <span className="font-bold text-xl tracking-tight">Scriptor</span>
         </div>
-        
-        <div className="flex-1 max-w-xl mx-12">
-          <div className="relative">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-stone-400 w-4 h-4" />
-            <input 
-              type="text" 
-              placeholder="Search your thoughts..." 
-              className="w-full bg-stone-100 border-none rounded-full py-2 pl-10 pr-4 focus:ring-2 focus:ring-stone-200 transition-all"
-            />
-          </div>
-        </div>
 
         <Link href={'/create'} className="flex items-center gap-2 bg-stone-900 text-stone-50 px-4 py-2 rounded-full hover:bg-stone-800 transition-colors">
           <Plus className="w-4 h-4" />
